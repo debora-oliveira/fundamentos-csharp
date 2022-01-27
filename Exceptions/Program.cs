0309b1c40176a75d41277393cf697ea860dbc237
Debora Oliveira da Silva <debora.oliveira.dev@gmail.com>
2022-01-27 16:49:09 -0300

    projeto exceptions

---
+++ Exceptions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[3];
+            //Testar("");
+            try
+            {
+                for (var i = 0; i < 5; i++)
+                {
+                    //IndexOutOfRangeException
+                    Console.WriteLine(array[i] = i);
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("indice não encontrado");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ocorreu um erro: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("sempre ocorre");
+            }
+        }
+
+        static void Testar(string texto)
+        {
+            if (string.IsNullOrEmpty(texto))
+                throw new Exception("O texto não pode ser nulo ou vazio");
+        }
+    }
+}
