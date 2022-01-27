31f46ac920020b1655f3df42d20e02772663d361
Debora Oliveira da Silva <debora.oliveira.dev@gmail.com>
2022-01-27 16:47:58 -0300

    projeto tipo moedas

---
+++ TiposMoedas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace TiposMoedas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            decimal valor = 10.24m;
+            //formatador numerico padrão
+            //C formata no padrão moeda
+            //P porcentagem
+            Console.WriteLine(valor.ToString(
+                "C",
+                CultureInfo.CreateSpecificCulture("pt-BR")
+                )
+            );
+
+            Console.WriteLine(Math.Round(valor));
+            Console.WriteLine(Math.Ceiling(valor));
+            Console.WriteLine(Math.Floor(valor));
+        }
+    }
+}
